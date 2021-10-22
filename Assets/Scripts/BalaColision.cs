703a9d9144e0a0b5d7a123210b9100f526f39da4
Kevin Aguilar <aguilarkevin825@gmail.com>
2021-10-22 10:03:57 -0400

    Colision bala con enemigo

---
+++ Assets/Scripts/BalaColision.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaColision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("obo");
+    }    
+}
